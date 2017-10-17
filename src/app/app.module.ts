be6c0a7799c82339012af39e702fcf1e4fc55976
Nikola Trajkovic <nikola.trajkovic@bluecouchdigital.com>
2017-10-17 22:32:51 +0200

    Added Clients table and Add client functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
-// AngularFire Imports
 import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabase } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
-// Component Imports
+import { AngularFireDatabase } from 'angularfire2/database';
+
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { ClientsComponent } from './components/clients/clients.component';
-import { ClientDetailsComponent } from './components/client-details/client-details.component';
 import { AddClientComponent } from './components/add-client/add-client.component';
+import { ClientDetailsComponent } from './components/client-details/client-details.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { EditClientComponent } from './components/edit-client/edit-client.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { RegisterComponent } from './components/register/register.component';
 import { SettingsComponent } from './components/settings/settings.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ClientService } from './services/client.service';
-// Service Imports
 
 const appRoutes: Routes = [
-  {path:'', component:DashboardComponent},
-  {path:'register', component:RegisterComponent},
-  {path:'login', component:LoginComponent}
+  { path:'', component: DashboardComponent },
+  { path:'register', component: RegisterComponent },
+  { path:'login', component: LoginComponent },
+  { path:'add-client', component: AddClientComponent }
 ];
 
 export const firebaseConfig = {
--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -9,14 +9,25 @@ import { Client } from '../../models/client';
 })
 export class ClientsComponent implements OnInit {
   clients: Client[];
+  totalOwed: number;
 
-  constructor(public clientService: ClientService) { }
+  constructor(public clientService: ClientService) {
+   }
 
   ngOnInit() {
     this.clientService.getClients().subscribe(clients => {
       this.clients = clients;
-      console.log(this.clients);
+      this.getTotalOwed();
     });
+  }
+
+  getTotalOwed() {
+    let total = 0;
+    for (let i = 0; i < this.clients.length; i++) {
+      total += parseFloat(this.clients[i].balance);
+    }
+    this.totalOwed = total;
+    console.log(this.totalOwed);
   }
 
 }
